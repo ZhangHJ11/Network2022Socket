5654589b0bd5c45949dbd131ff1e09e5523f7a4a
JoeKerr <diablostark8@gmail.com>
2022-12-03 13:45:44 +0800

    REPush "Get"

--- Client/methods/Get.java
+++ Client/methods/Get.java
@@ -1,24 +1,60 @@
 package Client.methods;
 
 import Client.Connections;
+import Client.Requestmessage.HTTPRequest;
+import Client.Requestmessage.RequestHead;
+import Client.Requestmessage.RequestLine;
+import util.InputStreamReader;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.ConnectException;
+import java.net.Socket;
 
 public class Get implements RequestMethod {
-    String host;
-    int port;
-    Connections pool;
+    private String host;
+    private int port;
+    private Connections pool;
 
-    public Get(String host,int port,Connections pool){
-        this.host=host;
-        this.port=port;
-        this.pool=pool;
+    public Get(String host, int port, Connections pool) {
+        this.host = host;
+        this.port = port;
+        this.pool = pool;
     }
-    public void ConductResponse(InputStream inputStream,String s) throws IOException{
-        //实现 处理响应 的操作  对状态码做出处理
+
+
+    private HTTPRequest assembleRequest(String url,boolean isKeepAlive) {
+        RequestLine requestline = new RequestLine("GET", url);
+        RequestHead requestHead = new RequestHead();
+
+        requestHead.put("Accept", "*/*");
+        requestHead.put("Accept-Language", "zh-cn");
+        requestHead.put("User-Agent", "WeDoRay-HTTPClient");
+
+        if (port != 80 && port != 443) {
+            requestHead.put("Host", host + ':' + port);
+        } else {
+            requestHead.put("Host", host); // 访问默认端口的时候是不需要端口号的
+        }
+        requestHead.put("Connection", isKeepAlive?"Keep-Alive":"");
+
+        return new HTTPRequest(requestline, requestHead, null);
     }
-    public void SendRequest(String s,boolean isKeepAlive) throws IOException{
+
+    public void conductResponse(InputStream inputStream) throws IOException {
+        System.out.println(InputStreamReader.readAll(inputStream));
+
+    }
+
+    public void sendRequest(String url, boolean isKeepAlive) throws IOException {
         //实现发送请求
+        try(Socket server = new Socket(this.host, this.port)) {
+            HTTPRequest request = assembleRequest(url,isKeepAlive);
+            server.getOutputStream().write(request.toString().getBytes());
+
+        }
+        catch (ConnectException e) {
+            e.printStackTrace();
+        }
     }
 }
